8fcfc2da25b91cebb04bc041d26a4f1b0b8de145
Vlad Titanov <48718937+Dudosity@users.noreply.github.com>
2020-03-16 19:42:18 +0300

    favicon and little shudle logic and more
    favicon and little shudle logic and more

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -14,17 +14,30 @@ export class RoomsComponent implements OnInit {
 
   constructor(private api: APIService) { }
 
+
   ngOnInit(): void {
 
     this.api.getcian().subscribe(data => {
       this.rooms_object = data['items'];
-      console.log(this.rooms_object);
+      console.log(this.rooms_object.length);
     });
 
     this.api.getn1().subscribe(data =>{
       this.rooms_object_n1 = data['result'];
-      console.log(this.rooms_object_n1)
+      console.log(this.rooms_object_n1.length);
+      Array.prototype.push.apply(this.rooms_object, this.rooms_object_n1);
+      this.rooms_object = this.shuffle(this.rooms_object);
     });
+
   }
+
+    shuffle(list) {
+    return list.reduce((p, n) => {
+      const size = p.length;
+      const index = Math.trunc(Math.random() * (size - 1));
+      p.splice(index, 0, n);
+      return p;
+    }, []);
+  };
 
 }
